e6ac488b7c9dd423840fbdc0383a178fccfeb0ff
Rebeca <inscripciones225@gmail.com>
2025-04-28 19:30:49 -0400

    updates

--- src/modules/auth/services/authApiService.jsx
+++ src/modules/auth/services/authApiService.jsx
@@ -1,12 +1,15 @@
 import { apiClient } from "../../../shared/services/apiClient";
 
-export const loginService = async ( { correo, password }) => {
-    try {
-        const { data } = await apiClient.post('https://proyecto6-sgv2.onrender.com/api/v1/user/login', { correo, password })
-        console.log(data);
-        return data;
-    } catch (error) {
-        console.error("Error logging in:", error);
-        throw new Error(error);
-    }
-}
+export const loginService = async ({ correo, password }) => {
+  try {
+    const { data } = await apiClient.post(
+      "https://proyecto6-sgv2.onrender.com/api/v1/user/login",
+      { correo, password }
+    );
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error logging in:", error);
+    throw new Error(error);
+  }
+};
