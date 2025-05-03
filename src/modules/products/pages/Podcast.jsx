be5f2235f6ba0eb44ad390668e8c212df37b44b4
Rebeca <inscripciones225@gmail.com>
2025-05-03 03:34:29 -0400

    font updates

--- src/modules/products/pages/Podcast.jsx
+++ src/modules/products/pages/Podcast.jsx
@@ -6,14 +6,14 @@ export const Podcast = () => {
       <h2 className="text-5xl font-bold mt-20 mb-6 text-center text-white">
         Podcast: Con Sonido de Alabanza
       </h2>
-      <div className="grid grid-cols-2 gap-10 items-center justify-center gap-5 p-10">
+      <div className="grid lg:grid-cols-2 md:grid-cols-1 gap-10 items-center justify-center gap-5 p-10">
         <div className="custom-card bg-background/50 dark:bg-default-100/50 max-w-[610px]">
           <div className="mx-auto flex flex-row justify-center object-center bg-gradient-to-br from-[#f0f4f8] to-[#e2ebf3] rounded-lg px-1 py-2 sm:py-5 lg:max-w-7xl">
             <div className="flex flex-col items-center justify-center p-7">
-              <h3 className="font-bold text-white text-xl text-center md:my-6">
+              <h3 className="text-white text-center font-bold text-sm md:text-base lg:text-lg">
                 Con Sonido de Alabanza
               </h3>
-              <h4 className="text-white text-center text-xs uppercase md:text-sm lg:text-base">
+              <h4 className="text-white text-center text-xs md:text-base lg:text-lg">
                 Podcast #1
               </h4>
             </div>
@@ -190,10 +190,10 @@ export const Podcast = () => {
         <div className="custom-card bg-background/50 dark:bg-default-100/50 max-w-[610px]">
           <div className="mx-auto flex flex-row justify-center object-center bg-gradient-to-br from-[#f0f4f8] to-[#e2ebf3] rounded-lg px-1 py-2 sm:py-5 lg:max-w-7xl">
             <div className="flex flex-col items-center justify-center p-5">
-              <h3 className="font-bold text-white text-xl text-center md:my-6">
+              <h3 className="font-bold text-white text-xl text-center md:my-6 md:text-base sm:text-sm">
                 Con Sonido de Alabanza
               </h3>
-              <h4 className="text-white text-center text-xs uppercase md:text-sm lg:text-base">
+              <h4 className="text-white text-center text-xs uppercase md:text-base sm:text-xs">
                 Podcast #10
               </h4>
             </div>
