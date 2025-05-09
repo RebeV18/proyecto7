063b962ad20cc6e901e21453393349a71e01cddf
Rebeca <inscripciones225@gmail.com>
2025-05-09 00:18:48 -0400

    updates

--- src/modules/auth/context/AuthContext.jsx
+++ src/modules/auth/context/AuthContext.jsx
@@ -1,24 +1,18 @@
-// import { createContext } from "react";
-
-// export const AuthContext = createContext();
-
-
 import { createContext, useContext, useState } from "react";
- 
+
 export const AuthContext = createContext();
- 
+
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
- 
+
   const login = () => setIsAuthenticated(true);
   const logout = () => setIsAuthenticated(false);
- 
+
   return (
     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
- 
+
 export const useAuthContext = () => useContext(AuthContext);
- 
