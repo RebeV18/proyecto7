e6956d4d2e56174f0f7e55028afa5a9d2a46b819
Rebeca <inscripciones225@gmail.com>
2025-05-10 17:39:12 -0400

    route update

--- src/modules/auth/Hooks/useValidateRegisterForm.js
+++ src/modules/auth/Hooks/useValidateRegisterForm.js
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { registerRules } from "../Utils/registerRules";
+import { registerRules } from "../utils/registerRules";
 
 export const useValidateRegisterForm = (formData) => {
   const [errors, setErrors] = useState({});
