b67ff9f6bf79e114463c35387efda352a9fdee07
Rebeca <inscripciones225@gmail.com>
2025-05-17 20:44:45 -0400

    cache added to avoid delay to show discography

--- src/modules/products/services/productApiService.js
+++ src/modules/products/services/productApiService.js
@@ -1,6 +1,7 @@
 import { apiClient } from "../../../shared/services/apiClient";
+import { cache } from "react";
 
-export const fetchAllProducts = async () => {
+export const fetchAllProducts = cache(async () => {
   try {
     const { data } = await apiClient.get(
       "https://proyecto6-sgv2.onrender.com/api/v1/product/readall/"
@@ -10,9 +11,9 @@ export const fetchAllProducts = async () => {
     console.error("Error obteniendo los productos", error);
     throw new Error(error);
   }
-};
+});
 
-export const fetchProductById = async (id) => {
+export const fetchProductById = cache(async (id) => {
   try {
     const { data } = await apiClient.get(
       `https://proyecto6-sgv2.onrender.com/api/v1/product/readone/${id}`
@@ -22,4 +23,4 @@ export const fetchProductById = async (id) => {
     console.error(`Error obteniendo el producto con el id: ${id}`, error);
     throw new Error(error);
   }
-};
+});
